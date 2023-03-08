d7b2fe598be671093042c921873a37bdd6b86f76
NaraeJang <j.naaaarae@gmail.com>
2023-03-08 13:49:49 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+app.use(express.static('public'));
+
+
+
+
+
+/////Starting From Here/////
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+
+
+
+
+
+/////Port Starting Message/////
+app.listen(3000, function () {
+    console.log('Server started on Port 3000.');
+});
